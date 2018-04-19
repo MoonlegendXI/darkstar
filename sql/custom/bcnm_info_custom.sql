880a52f952fc7eb9a732dbb84369161686e142bd
Moon <infernalforest@gmail.com>
2018-04-19 00:51:07 -0400

    Dyna d stuff

--- sql/custom/bcnm_info_custom.sql
+++ sql/custom/bcnm_info_custom.sql
@@ -1,4 +1,4 @@
--- REPLACE INTO `bcnm_info` VALUES (2000,295,'dynamis-bastok_[D]','Mclovin',1,18,3600,0,64,0,7);
--- REPLACE INTO `bcnm_info` VALUES (2001,294,'dynamis-san_dOria_[D]','Mclovin',1,18,3600,0,64,0,7);
--- REPLACE INTO `bcnm_info` VALUES (2002,296,'dynamis-windurst_[D]','Mclovin',1,18,3600,0,64,0,7);
--- REPLACE INTO `bcnm_info` VALUES (2003,297,'dynamis-jeuno_[D]','Mclovin',1,18,3600,0,64,0,7);
+REPLACE INTO `bcnm_info` VALUES (1400,295,'dynamis-bastok_[D]','Mclovin',1,18,3600,0,64,0,7);
+REPLACE INTO `bcnm_info` VALUES (1401,294,'dynamis-san_dOria_[D]','Mclovin',1,18,3600,0,64,0,7);
+REPLACE INTO `bcnm_info` VALUES (1402,296,'dynamis-windurst_[D]','Mclovin',1,18,3600,0,64,0,7);
+REPLACE INTO `bcnm_info` VALUES (1403,297,'dynamis-jeuno_[D]','Mclovin',1,18,3600,0,64,0,7);
